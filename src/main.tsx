1d95192300d7bbc112db7bbea17afcf1ec9d8c6a
Rostyslav Brenych <brenychinfo@gmail.com>
2026-02-22 22:16:50 +0100

    Align import client with RLS defaults and i18n-safe placeholders

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { I18nProvider } from './i18n/i18n'
+import './style.css'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <I18nProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </I18nProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+)
